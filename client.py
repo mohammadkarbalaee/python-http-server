8c7c2bb0c5d8049e6b349de105b9a95145e16832
Mohammad Karbalaee <muhammad.ksht@gmail.com>
2024-05-16 16:48:17 +0330

    remove redundancies

--- client.py
+++ client.py
@@ -1,23 +1,16 @@
 import socket
 
-# Define server address
-SERVER_HOST = 'https://python-http-server-zntk.onrender.com'  # The server's hostname or IP address
-SERVER_PORT = 8080          # The port used by the server
+SERVER_HOST = '127.0.0.1' 
+SERVER_PORT = 8080         
 
-# Define HTTP GET request
 HTTP_REQUEST = "GET /index.html HTTP/1.1\r\nHost: localhost\r\n\r\n"
 
-# Function to send HTTP GET request to server
 def send_request():
-    # Create a socket object
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # Connect to server
         s.connect((SERVER_HOST, SERVER_PORT))
         
-        # Send HTTP GET request to server
         s.sendall(HTTP_REQUEST.encode())
 
-        # Receive and print data from server
         while True:
             data = s.recv(1024)
             if not data:
--- server.py
+++ server.py
@@ -1,19 +1,17 @@
 import socket
 import threading
 
-# Define host and port
-HOST = '100.20.92.101'  # Standard loopback interface address (localhost)
-PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
+HOST = '127.0.0.1' 
+PORT = 8080     
 
-# Read the content of index.html
+
 with open("index.html", "r") as file:
     INDEX_HTML_CONTENT = file.read()
 
-# Read the content of 404.html
 with open("404.html", "r") as file:
     NOT_FOUND_HTML_CONTENT = file.read()
 
-# Define HTTP response
+
 HTTP_RESPONSE_OK = f"""\
 HTTP/1.1 200 OK
 
@@ -26,41 +24,30 @@ HTTP/1.1 404 Not Found
 {NOT_FOUND_HTML_CONTENT}
 """
 
-# Function to handle client requests
 def handle_client(conn, addr):
     print(f"Connected by {addr}")
 
-    # Receive data from client
     data = conn.recv(1024)
     if data:
-        # Print details of the HTTP request
         print(f"Request from {addr}:\n{data.decode()}")
 
         if data.decode().startswith("GET /index.html"):
-            # Send HTTP response with index.html content to client
             conn.sendall(HTTP_RESPONSE_OK.encode())
         else:
-            # Send a 404 response for requests other than index.html
             conn.sendall(HTTP_RESPONSE_NOT_FOUND.encode())
 
-    # Close connection
     conn.close()
     print(f"Connection closed by {addr}")
 
-# Create a socket object
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # Bind the socket to the address
     s.bind((HOST, PORT))
     
-    # Listen for incoming connections
     s.listen()
 
     print(f"Server listening on {HOST}:{PORT}")
 
     while True:
-        # Accept connections from clients
         conn, addr = s.accept()
 
-        # Create a new thread to handle the client
         client_thread = threading.Thread(target=handle_client, args=(conn, addr))
         client_thread.start()
